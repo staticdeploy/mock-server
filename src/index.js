8718a2a9a8c895b31d22bcac1420a1fe5c8519ff
Davide Bianchi <bianchidavide12@gmail.com>
2017-04-21 14:36:08 +0200

    [fix]: add recursive true to node-watch

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const {basename} = require("path");
 const getApp = require("./getApp");
 
 module.exports = function startServer (options) {
-    const {root, watch, port, requiredModules} = options;
-    requiredModules.forEach(require);
+    const {root, watch, port} = options;
+    options.require.forEach(require);
     const server = createServer()
         .addListener("request", getApp(options))
         .listen(port, () => {
@@ -20,7 +20,7 @@ module.exports = function startServer (options) {
         // means replacing the listener for the request event. We replace the
         // old app, created with the old configuration, with the new app,
         // created with the new configuration.
-        fsWatch(root, debounce(() => {
+        fsWatch(root, {recursive: true}, debounce(() => {
             log(`Change detected in directory ${green(basename(root))}, reconfiguring ${cyan("sd-mock-server")}`);
             server
                 .removeAllListeners("request")
